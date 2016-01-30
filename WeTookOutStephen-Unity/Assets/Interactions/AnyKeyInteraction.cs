5b8a19048323741e3ad44099d9cd632b2758a975
Mangatome <Mangatome@users.noreply.github.com>
2016-01-30 15:58:08 +0100

    Interactions folder structure

---
+++ WeTookOutStephen-Unity/Assets/Interactions/AnyKeyInteraction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnyKeyInteraction : InteractionBase {
+
+	void Update () {
+
+        if (Input.anyKeyDown)
+        {
+            base.OnInteractionSuccess();
+        }
+	}
+}
--- WeTookOutStephen-Unity/Assets/Interactions/InteractionBase.cs
+++ WeTookOutStephen-Unity/Assets/Interactions/InteractionBase.cs
@@ -5,6 +5,9 @@ public class InteractionBase : MonoBehaviour {
 
     protected void OnInteractionSuccess()
     {
+        // TODO: Disables this object for now.
+        this.gameObject.SetActive(false);
+        
         // TODO: this will alert the GameState and trigger some scene changes
     }
 	
