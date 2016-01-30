37f750a799b0f56e1f3c02c09909b6662e6a3253
Asthmet <asthmet@gmail.com>
2016-01-30 18:31:36 +0100

    single key interactions classes

---
+++ WeTookOutStephen-Unity/Assets/Interactions/SpecificKeyInteraction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpecificKeyInteraction : InteractionBase {
+
+    public KeyCode SpecificKey;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKey(SpecificKey))
+            {
+                base.OnInteractionSuccess();
+            }
+        }
+	}
+}
