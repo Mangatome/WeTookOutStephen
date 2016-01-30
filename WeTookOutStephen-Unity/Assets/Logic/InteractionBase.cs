eca9f8aa45350ee2f65a84d989b3bed7e8cb6d20
Mangatome <Mangatome@users.noreply.github.com>
2016-01-30 15:17:16 +0100

    Base classes for game-wide logic

---
+++ WeTookOutStephen-Unity/Assets/Logic/InteractionBase.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionBase : MonoBehaviour {
+
+    protected void OnInteractionSuccess()
+    {
+        // TODO: this will alert the GameState and trigger some scene changes
+    }
+	
+}
---
+++ WeTookOutStephen-Unity/Assets/Model/GameState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+public class GameState {
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the singleton game state.
+    /// </summary>
+    public static GameState Instance
+    {
+        get
+        {
+            return _Instance ?? (_Instance = new GameState());
+        }
+    }
+
+    #endregion
+
+    #region Fields
+
+    private static GameState _Instance;
+
+    #endregion
+
+    #region Constructors
+
+    private GameState()
+    {
+    }
+
+    #endregion
+}
