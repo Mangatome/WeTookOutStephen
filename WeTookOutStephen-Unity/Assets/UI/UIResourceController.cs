6c5855127d13fa236eeb86e12d80aa4f9a1afb25
Asthmet <asthmet@gmail.com>
2016-01-31 01:17:55 +0100

    UI scripts

---
+++ WeTookOutStephen-Unity/Assets/UI/UIResourceController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class UIResourceController : MonoBehaviour {
+
+    private int maxValue = 10;
+    private int currentValue = 0; 
+    public Text txtRef;
+    public Image ImgRef;
+
+    // Use this for initialization
+    void Start ()
+    {
+        // currentValue = Resource.getCurrentValue()
+        // ImgRef = Resource.getImg()
+        setCountText();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // place here the getter method for the value of the resource
+        // currentValue = Resource.getCurrentValue()
+        // maxValue = Resource.getMaxValue()
+        setCountText();
+    }
+
+    void setCountText()
+    {
+        txtRef.text = currentValue + "/" + maxValue;
+    }
+}
---
+++ WeTookOutStephen-Unity/Assets/UI/UITimeController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class UITimeController : MonoBehaviour {
+
+    private float timeCount;
+    public Text txtRef;
+
+    // Use this for initialization
+    void Start ()
+    {
+        timeCount = 0;
+        setCountText();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timeCount = Time.time;
+        setCountText();
+    }
+
+    void setCountText()
+    {
+        txtRef.text = "Time: " + timeCount.ToString().Split('.')[0];
+    }
+}
