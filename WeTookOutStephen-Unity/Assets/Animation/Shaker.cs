c1a45ba47561f4aaf460ce17235c115556721d60
Tomas Balyo <biotomas@gmail.com>
2016-01-31 14:25:03 +0100

    basic cheap animations

---
+++ WeTookOutStephen-Unity/Assets/Animation/Shaker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shaker : MonoBehaviour {
+
+    public float magnitude = 0.2f;
+    public int skipFrames = 5;
+
+    private int frame;
+    private System.Random rnd = new System.Random();
+    private Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+        position = transform.position;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        frame++;
+        if (frame % skipFrames == 0)
+        {
+            transform.Translate((float)rnd.NextDouble() * magnitude - (magnitude / 2),
+                (float)rnd.NextDouble() * magnitude - (magnitude / 2), 0);
+            if (rnd.Next(2) == 0)
+            {
+                transform.position = position;
+            }
+        }
+    }
+}
---
+++ WeTookOutStephen-Unity/Assets/Animation/Wobbler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wobbler : MonoBehaviour {
+
+    public float maxAngle = 20;
+    public float speed = 0.2f;
+    private float nowAngle = 0;
+
+	// Use this for initialization
+	void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (nowAngle < -maxAngle || nowAngle > maxAngle)
+        {
+            speed = -speed;
+        }
+        nowAngle += speed;
+        transform.Rotate(0, 0, speed);
+	}
+}
