d6feef293b41207382b9c8adfd6839bf3096db6b
Tomas Balyo <biotomas@gmail.com>
2016-01-31 15:25:57 +0100

    SoundEffect added, music returned

--- WeTookOutStephen-Unity/Assets/Music/SoundEffectMixer.cs
+++ WeTookOutStephen-Unity/Assets/Music/SoundEffectMixer.cs
@@ -8,18 +8,6 @@ public class SoundEffectMixer : MonoBehaviour {
     public AudioClip zoomClip;
     public AudioClip[] clips = new AudioClip[10];
 
-    private System.Random rnd = new System.Random();
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     public void playRandom()
     {
         asource.PlayOneShot(clips.RandomOrDefault());
