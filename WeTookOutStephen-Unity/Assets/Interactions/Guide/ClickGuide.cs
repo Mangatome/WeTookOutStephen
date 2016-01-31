da57358692d4425d166459b7043c94ce32f2ad48
Asthmet <asthmet@gmail.com>
2016-01-31 14:58:22 +0100

    interaction guides

---
+++ WeTookOutStephen-Unity/Assets/Interactions/Guide/ClickGuide.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickGuide : MonoBehaviour {
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 rotate;
+        rotate.x = 0;
+        rotate.y = 0;
+        rotate.z = -2;
+        transform.Rotate(rotate);
+        
+    }
+}
