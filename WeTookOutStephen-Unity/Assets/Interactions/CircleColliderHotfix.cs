bf5fc215dc6b4307a0caf90cff1f35b969ac2bdc
Mangatome <Mangatome@users.noreply.github.com>
2016-01-31 16:10:18 +0100

    Fixed some colliders scale down (hack fix!)

---
+++ WeTookOutStephen-Unity/Assets/Interactions/CircleColliderHotfix.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class CircleColliderHotfix : MonoBehaviour {
+
+    private Vector3 _startLocalScale;
+    private float _startRadius;
+    private CircleCollider2D _collider2D;
+
+	// Use this for initialization
+	void Start () {
+        _collider2D = GetComponent<CircleCollider2D>();
+        _startLocalScale = transform.localScale;
+        _startRadius = _collider2D.radius;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float s = transform.localScale.magnitude / _startLocalScale.magnitude;
+        _collider2D.radius = _startRadius / s;
+	}
+}
